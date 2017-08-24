22d9bd3d148c97b35075652f5af62d4bea0cac0e
Johananu <jaqueline-johana@outlook.com>
2017-08-24 09:26:54 -0600

    Initial commit adding structure of directories and creating the first file

---
+++ www/js/app.js
@@ -0,0 +1,23 @@
+var base=5;
+var altura=7;
+var area= base*altura;
+console.log("el area de mi rectangulo es igual: "+area)
+//   el area de mi rectangulo es igual: 35
+
+var radio=8;
+var pi=3.1416;
+var area=radio^2*pi;
+console.log("el area de mi circulo es igual: "+area)
+//el area de mi circulo es igual: 201.0624
+
+var base=4;
+var altura=8;
+var area=base*altura/2;
+console.log("el area de mi triangulo es igual: "+area)
+//el area de mi triangulo es igual: 16
+
+var radio=6;
+var pi=3.1416;
+var volumen=(4/3)*pi*radio^3;
+console.log("el volumen de mi esfera es igual: "+volumen)
+//el volumen de mi esfera es igual: 26
